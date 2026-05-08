a24030ebb745b7746d22836991a14fcec8301e90
Stockton Erickson <143432670+stocke12@users.noreply.github.com>
2026-05-08 15:07:19 -0600

    Update step_trainer_landing.sql

--- step_trainer_landing.sql
+++ step_trainer_landing.sql
@@ -11,5 +11,5 @@ WITH SERDEPROPERTIES (
   'mapping' = 'TRUE'
 )
 STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://stedi-lakehouse-stockton/step_trainer_landing/'
+LOCATION 's3://stedi-lakehouse-stockton/step_trainer/landing/'
 TBLPROPERTIES ('classification' = 'json');
